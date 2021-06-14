bd7a2892513a9b2f1bb770deffa7c3ff86ad2080
takano <ri1saxxminny83xx@gmail.com>
2021-06-14 23:21:30 +0900

    一時保存2

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Layout({children, title = "HP by Nextjs"}) {}
+export default function Layout({children, title = "HP by Nextjs"}) {
+    return (
+    <div className="flex justify-center item-center flex-col min-h-screen text-gray-600 text-sm font-mono"></div>
+);
+}
