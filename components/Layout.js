c25cc14fae7b4fc510d5998c372f369c67468652
takano <ri1saxxminny83xx@gmail.com>
2021-06-15 23:41:18 +0900

    Layout component作成

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,15 @@ import Link from "next/link";
 
 export default function Layout({children, title = "HP by Nextjs"}) {
     return (
-    <div className="flex justify-center item-center flex-col min-h-screen text-gray-600 text-sm font-mono"></div>
+    <div className="flex justify-center item-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+        <Head>
+            <title>
+                {title}
+            </title>
+            <header>
+                <nav className="bg-gray-800 w-screen"></nav>
+            </header>
+        </Head>
+    </div>
 );
 }
